781dc82c342ff0e9b1bb6dfc420177421fd6a0ab
adarshnarayanhegde <adarshnarayanhegde@gmail.com>
2020-06-11 11:08:08 -0400

    initial limits service and spring cloud config server

---
+++ limits-service/src/main/java/com/adarsh/learn/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,19 @@
+package com.adarsh.learn.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adarsh.learn.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsConfigurations() {
+		return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
+	}
+}

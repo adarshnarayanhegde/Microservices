d78c4970a9b03e990ae15d28a4ad83bde0e28e0e
adarshnarayanhegde <adarshnarayanhegde@gmail.com>
2020-06-15 11:48:17 -0400

    Eureka naming server and dynamic load balancing using Ribbon

---
+++ currency-exchange-service/src/main/java/com/adarsh/learn/microservices/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,8 @@
+package com.adarsh.learn.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long>{
+
+	ExchangeValue findByFromAndTo(String from, String to);
+}
